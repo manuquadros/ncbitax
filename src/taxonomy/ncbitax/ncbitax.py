a656d63974a541b23613fbabe953d44e97cc3752
Emanuel Quadros <emanuel.quadros@dsmz.de>
2025-04-17 17:39:57 +0200

    feat: add load_df, generalizing the access to tables in the NCBI dump

--- src/taxonomy/ncbitax/ncbitax.py
+++ src/taxonomy/ncbitax/ncbitax.py
@@ -5,14 +5,17 @@ import pandas as pd
 from typing import IO, cast
 
 ROOTDIR = pathlib.Path(__file__).parent.parent.parent.parent
+taxdump = ROOTDIR / "resources/taxdump.tar.gz"
 
 
 class NCBIDump(TextIOBase):
-    def __init__(self, file: str | tarfile.ExFileObject):
-        if isinstance(file, tarfile.ExFileObject):
-            self._file: IO[str] = TextIOWrapper(file, encoding="utf-8")
-        else:
-            self._file = open(file, "rt")
+    def __init__(self, table: str):
+        if table[:-4] != ".dmp":
+            table = table + ".dmp"
+
+        self._file = TextIOWrapper(
+            tarfile.open(taxdump).extractfile(table), encoding="utf-8"
+        )
 
     def readline(self, *args):
         line = self._file.readline(*args)
@@ -29,28 +32,80 @@ class NCBIDump(TextIOBase):
         self._file.close()
 
 
-def names_dataframe() -> pd.DataFrame:
-    names_stream = cast(
-        tarfile.ExFileObject,
-        tarfile.open(ROOTDIR / "resources/taxdump.tar.gz").extractfile(
-            "names.dmp"
-        ),
-    )
-    filepath = ROOTDIR / "resources/names.parquet.zst"
+def colnames(table: str) -> str:
+    """Return the column names for each file (table) in the NCBI dump."""
+    match table:
+        case "names":
+            return ("tax_id", "name_txt", "unique_name", "name_class")
+        case "nodes":
+            return (
+                "tax_id",
+                "parent_tax_id",
+                "rank",
+                "embl_code",
+                "division_id",
+                "inherited_div_flag",
+                "genetic_code_id",
+                "inherited_gc_flag",
+                "mitochondrial_genetic_code",
+                "inherited_mgc_flag",
+                "genbank_hidden_flag",
+                "hidden_subtree_root_flag",
+                "comments",
+            )
+        case "division":
+            return ("division_id", "division_cde", "division_name", "comments")
+        case "gencode":
+            return ("genetic_code_id", "abbreviation", "name", "cde", "starts")
+        case "delnodes":
+            return ("tax_id",)
+        case "merged":
+            return ("old_tax_id", "new_tax_id")
+        case "citations":
+            return (
+                "cit_id",
+                "cit_key",
+                "medline_id",
+                "pubmed_id",
+                "url",
+                "text",
+            )
+        case "images":
+            return (
+                "image_id",
+                "image_key",
+                "url",
+                "license",
+                "attribution",
+                "source",
+                "properties",
+                "taxid_list",
+            )
+
+
+def load_df(table: str) -> pd.DataFrame:
+    if table[:-4] == ".dmp":
+        table = table[:4]
+
+    filepath = ROOTDIR / f"resources/{table}.parquet.zst"
 
     try:
         return pd.read_parquet(filepath, engine="pyarrow")
     except FileNotFoundError:
-        with NCBIDump(names_stream) as names:
+        with NCBIDump(table) as table:
             df = pd.read_csv(
-                names,
+                table,
                 sep=r"\t\|\t",
                 engine="python",
                 header=None,
-                names=("tax_id", "name_txt", "unique_name", "name_class"),
+                names=colnames(table),
             )
 
         df.to_parquet(
             filepath, engine="pyarrow", compression="zstd", index=False
         )
         return df
+
+
+def names_df() -> pd.DataFrame:
+    return load_df("names")
