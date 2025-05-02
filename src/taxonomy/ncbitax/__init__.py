ac82eb895a73909e22592eee60d1004012ce71a6
Emanuel Quadros <emanuel.quadros@dsmz.de>
2025-05-03 00:27:23 +0200

    chore: add missing exports

--- src/taxonomy/ncbitax/__init__.py
+++ src/taxonomy/ncbitax/__init__.py
@@ -1,19 +1,23 @@
 from .ncbitax import (
     bacterial_name_index,
     decompose_name,
+    DecomposedName,
     is_bacteria,
     is_bacterial_strain,
     names,
     nodes,
     normalize,
+    resolve_tax_id,
 )
 
 __all__ = [
     "bacterial_name_index",
     "decompose_name",
+    "DecomposedName",
     "is_bacterial_strain",
     "is_bacteria",
     "names",
     "nodes",
     "normalize",
+    "resolve_tax_id",
 ]
