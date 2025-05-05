6eee3354c03e6b573eef3d484aa1dc99fd308f42
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2025-05-05 14:58:22 +0200

    chore: clean up imports in __init__.py

--- src/taxonomy/ncbitax/__init__.py
+++ src/taxonomy/ncbitax/__init__.py
@@ -1,29 +1,18 @@
 import pandas as pd
-
-pd.options.mode.copy_on_write = True
-
 from .ncbitax import (
-    bacterial_name_index,
     decompose_name,
-    DecomposedName,
+    resolve_tax_id,
     is_bacteria,
     is_bacterial_strain,
-    names,
-    nodes,
-    normalize,
-    remove_citations,  # temp
-    resolve_tax_id,
+    DecomposedName,
 )
 
 __all__ = [
-    "bacterial_name_index",
     "decompose_name",
-    "DecomposedName",
-    "is_bacterial_strain",
-    "is_bacteria",
-    "names",
-    "nodes",
-    "normalize",
-    "remove_citations",  # temp
     "resolve_tax_id",
+    "is_bacteria",
+    "is_bacterial_strain",
+    "DecomposedName",
 ]
+
+pd.options.mode.copy_on_write = True
